6405173d5f915e793d4aee5a76153f4cefd27657
Joe Garcia, CISSP <joe.garcia@cyberark.com>
2018-03-19 08:09:09 -0400

    Fixed post-configuration

--- 0-start.sh
+++ 0-start.sh
@@ -6,31 +6,29 @@ echo '--------- Bring Up Conjur ------------'
 echo
 
 docker-compose up -d conjur
-docker-compose logs -f conjur
 
 echo
 echo '--------- Configure Conjur ------------'
 echo
 
-docker-compose exec conjur \
-  evoke configure master -h conjur -p Cyberark1 demo
+docker exec conjur-master \
+  evoke configure master -h conjur-master -p Cyberark1 demo
 
-docker cp conjur:/opt/conjur/etc/ssl/ca.pem ./certs
+docker cp conjur-master:/opt/conjur/etc/ssl/ca.pem ./certs
+
 openssl x509 -in ./certs/ca.pem -inform PEM -out ./certs/ca.crt
-
-api_key=$(docker-compose exec conjur sudo -u conjur conjur-plugin-service possum rails r "print Credentials['demo:user:admin'].api_key" | tail -1)
 
 echo
 echo '--------- Bring Up Conjur CLI ------------'
 echo
 
-docker-compose up -e CONJUR_AUTHN_API_KEY=$api_key -d conjur-cli
+docker-compose up -d conjur-cli
 
 echo
 echo '--------- Load Policy & AES256 Key ------------'
 echo
 
-docker-compose exec conjur-cli /bin/bash -c "
+docker exec -it conjur-cli /bin/bash -c "
   cp /src/certs/ca.crt /usr/local/share/ca-certificates/ca.crt
   update-ca-certificates
   conjur policy load --replace root /src/policies/aws-sse-c-policy.yml
